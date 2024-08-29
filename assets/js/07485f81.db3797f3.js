5e3531e9df5a0129b33156b2b6140de49a4f4a47
Roberto vargas <robertovargashdez@gmail.com>
2024-08-28 20:25:12 -0400

    Deploy website - based on 6d4537395ac7ae5d55b1a2b9f62f906cda00d82c

---
+++ assets/js/07485f81.db3797f3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkxceed_docs_words=self.webpackChunkxceed_docs_words||[]).push([[5460],{3163:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"zip"}')}}]);
---
+++ assets/js/5e112ca9.2c4c249c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkxceed_docs_words=self.webpackChunkxceed_docs_words||[]).push([[228],{6049:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"workbooks"}')}}]);
