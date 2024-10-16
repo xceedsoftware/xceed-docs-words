8e61d67e11c94382ea0a21f613dedb2017b41338
Roberto vargas <robertovargashdez@gmail.com>
2024-10-16 10:52:44 -0400

    some change

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,7 +3,7 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'Xceed Libraries for .NET',
+  title: 'Xceed Libraries for .NET test',
   tagline: 'Xceed Libraries for .NET documentation',
   favicon: 'img/logox.ico',
 
