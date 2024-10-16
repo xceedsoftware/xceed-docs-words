6ee3a9dde2d897765b77e6d0bb6a7bcf7eef6904
xceedsoftware <support@xceed.com>
2024-10-16 10:58:08 -0400

    ddd

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,7 +3,7 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'Xceed Libraries for .NET test',
+  title: 'Xceed Libraries for .NET testd',
   tagline: 'Xceed Libraries for .NET documentation',
   favicon: 'img/logox.ico',
 
